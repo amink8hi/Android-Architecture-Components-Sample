7329eda32e5d2dda3b93bc66eb8b02590061a27c
Amin Koohi <koohi@iccmail.ir>
2020-08-05 12:39:01 +0430

    @ bug fix

--- app/src/main/java/ir/yara/batman/di/module/ToastModule.kt
+++ app/src/main/java/ir/yara/batman/di/module/ToastModule.kt
@@ -4,19 +4,17 @@ import android.content.Context
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.components.ApplicationComponent
-import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.android.components.ActivityComponent
+import dagger.hilt.android.qualifiers.ActivityContext
 import ir.yara.batman.ui.view.customs.KitToast
-import javax.inject.Singleton
 
 
 @Module
-@InstallIn(ApplicationComponent::class)
+@InstallIn(ActivityComponent::class)
 class ToastModule {
 
     @Provides
-    @Singleton
-    fun toastProvider(@ApplicationContext context: Context): KitToast {
+    fun toastProvider(@ActivityContext context: Context): KitToast {
         return KitToast(context)
     }
 }
